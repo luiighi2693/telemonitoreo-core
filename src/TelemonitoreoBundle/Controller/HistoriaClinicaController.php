6e4fb8d64bba0fe08da6417ebcc0034d13a01558
luighi2693 <luiscarvajal2693@gmail.com>
2016-10-16 22:40:12 -0400

    controlador de historia clinica

--- src/TelemonitoreoBundle/Controller/HistoriaClinicaController.php
+++ src/TelemonitoreoBundle/Controller/HistoriaClinicaController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
 use AppBundle\Entity\User;
+use TelemonitoreoBundle\Entity\HistoriaClinica;
 
 class HistoriaClinicaController extends FOSRestController{
 
@@ -22,5 +23,67 @@ class HistoriaClinicaController extends FOSRestController{
      */
     public function getAllAction(){
         return $this->getDoctrine()->getRepository('TelemonitoreoBundle:HistoriaClinica')->findAll();
+    }
+
+    /**
+     * @Rest\GET("/historiaclinica/{id}")
+     */
+    public function getHistoriaClinicaAction($id){
+       return $this->getDoctrine()->getRepository("TelemonitoreoBundle:HistoriaClinica")->find($id);
+    }
+
+    /**
+     * @Rest\POST("/historiaclinica/")
+     */
+    public function addHistoriaClinicaAction(Request $request){
+        $data = new HistoriaClinica();
+        $data->setCodigo($request->headers->get("codigo"));
+        $data->setCedulaPaciente($request->headers->get("cedula"));
+        $data->setNombrePaciente($request->headers->get("nombre"));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        return new View("Historia clinica added succefully", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\PUT("/historiaclinica/{id}")
+     */
+    public function updateHistoriaClinicaAction($id, Request $request){
+        $nombre = $request->headers->get("nombre");
+        $cedula = $request->headers->get("cedula");
+        $codigo = $request->headers->get("codigo");
+
+        $em = $this->getDoctrine()->getManager();
+        $historiaClinica = $this->getDoctrine()->getRepository("TelemonitoreoBundle:HistoriaClinica")->find($id);
+        if(empty($historiaClinica)){
+            return new View("historia clinica not found", Response::HTTP_NOT_FOUND);
+        }else{
+            if(!empty($nombre)){
+                $historiaClinica->setNombrePaciente($nombre);
+            }
+            if(!empty($cedula)){
+                $historiaClinica->setCedulaPaciente($cedula);
+            }
+            if(!empty($codigo)){
+                $historiaClinica->setCodigo($codigo);
+            }
+
+            $em->flush();
+            return new View("Historia clinica Updated Successfully", Response::HTTP_OK);
+        }
+    }
+
+    /**
+     * @Rest\DELETE("/historiaclinica/{id}")
+     */
+    public function deleteHistoriaClinica($id){
+        $em = $this->getDoctrine()->getManager();
+        $historiaClinica = $this->getDoctrine()->getRepository("TelemonitoreoBundle:HistoriaClinica")->find($id);
+        $em->remove($historiaClinica);
+        $em->flush();
+        return new View("deleted succefully", Response::HTTP_OK);
     }
 }
