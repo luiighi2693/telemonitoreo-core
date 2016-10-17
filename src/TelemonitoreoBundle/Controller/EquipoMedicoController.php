4f661e07f86ae4032da55d001f3dfc38b7a471ce
luiighi2693 <luiscarvajal2693@gmail.com>
2016-10-17 12:16:55 -0400

    controlador de equipo medico

--- src/TelemonitoreoBundle/Controller/EquipoMedicoController.php
+++ src/TelemonitoreoBundle/Controller/EquipoMedicoController.php
@@ -13,6 +13,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
+use TelemonitoreoBundle\Entity\EquipoMedico;
 
 class EquipoMedicoController extends FOSRestController{
 
@@ -27,8 +28,89 @@ class EquipoMedicoController extends FOSRestController{
      * @Rest\GET("/equipomedico/{id}")
      */
     public function getEquipoMedico($id){
-//        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->find($id);
-        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->findBy(array('idhistoriaclinica' => $id));
+        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->find($id);
+//        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->findBy(array('idhistoriaclinica' => $id));
     }
 
+    /**
+     * @Rest\POST("/equipomedico/")
+     */
+    public function addEquipoMedicoAction(Request $request){
+        $data = new EquipoMedico();
+        $data->setIdhistoriaclinica($request->headers->get("idHistoriaClinica"));
+        $data->setTipoequipo($request->headers->get("tipoEquipo"));
+        $data->setNombre($request->headers->get("nombre"));
+        $data->setMarca($request->headers->get("marca"));
+        $data->setModelo($request->headers->get("modelo"));
+        $data->setTipoconexion($request->headers->get("tipoConexion"));
+        $data->setIp($request->headers->get("ip"));
+        $data->setModuloconexion($request->headers->get("moduloConexion"));
+        $data->setSerial($request->headers->get("serial"));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        return new View("Equipo medico added succefully", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\PUT("/equipomedico/{id}")
+     */
+    public function updateEquipoMedicoAction($id, Request $request){
+        $tipoEquipo = $request->headers->get("tipoEquipo");
+        $nombre = $request->headers->get("nombre");
+        $marca = $request->headers->get("marca");
+        $modelo = $request->headers->get("modelo");
+        $tipoConexion = $request->headers->get("tipoConexion");
+        $ip = $request->headers->get("ip");
+        $moduloConexion = $request->headers->get("moduloConexion");
+        $serial = $request->headers->get("serial");
+
+        $em = $this->getDoctrine()->getManager();
+        $equipoMedico = $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->find($id);
+
+        if (empty($equipoMedico)){
+            return new View("equipo medico not found", Response::HTTP_NOT_FOUND);
+        }else{
+            if(!empty($tipoEquipo)){
+                $equipoMedico->setTipoequipo($tipoEquipo);
+            }
+            if(!empty($nombre)){
+                $equipoMedico->setNombre($nombre);
+            }
+            if(!empty($marca)){
+                $equipoMedico->setMarca($marca);
+            }
+            if(!empty($modelo)){
+                $equipoMedico->setModelo($modelo);
+            }
+            if(!empty($tipoConexion)){
+                $equipoMedico->setTipoconexion($tipoConexion);
+            }
+            if(!empty($ip)){
+                $equipoMedico->setIp($ip);
+            }
+            if(!empty($moduloConexion)){
+                $equipoMedico->setModuloconexion($moduloConexion);
+            }
+            if(!empty($serial)){
+                $equipoMedico->setSerial($serial);
+            }
+
+            $em->flush();
+            return new View("equipo medico Updated Successfully", Response::HTTP_OK);
+        }
+    }
+
+    /**
+     * @Rest\DELETE("/equipomedico/{id}")
+     */
+    public function deleteEquipoMedicoAction($id){
+        $em = $this->getDoctrine()->getManager();
+        $equipoMedico = $this->getDoctrine()->getRepository("TelemonitoreoBundle:EquipoMedico")->find($id);
+        $em->remove($equipoMedico);
+        $em->flush();
+        return new View("deleted succefully", Response::HTTP_OK);
+    }
 }
