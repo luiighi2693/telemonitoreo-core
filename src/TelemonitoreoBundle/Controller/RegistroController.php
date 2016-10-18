48320a7d159d4e5edf0af08192d1f37ef851a0f9
luighi2693 <luiscarvajal2693@gmail.com>
2016-10-18 12:26:04 -0400

    controller de registro

--- src/TelemonitoreoBundle/Controller/RegistroController.php
+++ src/TelemonitoreoBundle/Controller/RegistroController.php
@@ -13,11 +13,52 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
+use TelemonitoreoBundle\Entity\Registro;
 
 class RegistroController extends FOSRestController{
 
     /**
      * @Rest\GET("/registro")
      */
-    
+    public function getAllAction(){
+        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:Registro")->findAll();
+    }
+
+    /**
+     * @Rest\GET("/registro/{id}")
+     */
+    public function getRegistroAction($id){
+        return $this->getDoctrine()->getRepository("TelemonitoreoBundle:Registro")->find($id);
+    }
+
+    /**
+     * @Rest\POST("/registro/")
+     */
+    public function addRegistro(Request $request){
+        $data = new Registro();
+        $data->setIdequipo($request->headers->get("idEquipo"));
+        $data->setfecha(new \DateTime($request->headers->get("fecha")));
+        $data->setDuracion(new \DateTime($request->headers->get("duracion")));
+        $data->setTipoarchivo($request->headers->get("tipoArchivo"));
+        $data->setUriarchivo($request->headers->get("uriArchivo"));
+        $data->setModulovisualizacion($request->headers->get("moduloVisualizacion"));
+        $data->setIdhistoriaclinica($request->headers->get("idHistoriaClinica"));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        return new View("Historia clinica added succefully", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\DELETE("/registro/{id}")
+     */
+    public function deleteRegistro($id){
+        $em = $this->getDoctrine()->getManager();
+        $registro = $this->getDoctrine()->getRepository("TelemonitoreoBundle:Registro")->find($id);
+        $em->remove($registro);
+        $em->flush();
+        return new View("deleted succefully", Response::HTTP_OK);
+    }
 }
